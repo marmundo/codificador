7122fabe414fe0a42ac7bd1077815995d223e720
Marcelo Damasceno de Melo <1725032@ifrn.local>
2018-06-20 17:33:24 -0300

    add mapeamento

---
+++ js/hash.js
@@ -0,0 +1,6 @@
+var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
+for(var i = 0; i < hashParams.length; i++){
+    var p = hashParams[i].split('=');
+    console.log(p)
+    document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+}
